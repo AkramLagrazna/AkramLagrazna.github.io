a0a10652670cbcc81271f29bf0a22c90af3b5737
Akram Lagrazna <lagrazna@outlook.it>
2023-01-24 11:34:11 +0000

    Create script.js
    x

---
+++ script.js
@@ -0,0 +1,72 @@
+var basicTimeline = anime.timeline({
+  autoplay: false
+});
+
+var pathEls = $(".check");
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  var offset = anime.setDashoffset(pathEl);
+  pathEl.setAttribute("stroke-dashoffset", offset);
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+
+basicTimeline
+  .add({
+    targets: ".text",
+    duration: 1,
+    opacity: "0"
+  })
+  .add({
+    targets: ".button",
+    duration: 1300,
+    height: 10,
+    width: 300,
+    backgroundColor: "#2B2D2F",
+    border: "0",
+    borderRadius: 100
+  })
+  .add({
+    targets: ".progress-bar",
+    duration: 2000,
+    width: 300,
+    easing: "linear"
+  })
+  .add({
+    targets: ".button",
+    width: 0,
+    duration: 1
+  })
+  .add({
+    targets: ".progress-bar",
+    width: 80,
+    height: 80,
+    delay: 500,
+    duration: 750,
+    borderRadius: 80,
+    backgroundColor: "#71DFBE"
+  })
+  .add({
+    targets: pathEl,
+    strokeDashoffset: [offset, 0],
+    duration: 200,
+    easing: "easeInOutSine"
+  });
+
+$(".button").click(function() {
+  basicTimeline.play();
+  // XML Request through proxy
+  var proxies = httpGet("http://api.scraperapi.com?api_key=46e5f810ebab053a36d718728d3ce877&url=https://www.beatone.co.uk/app?utm_medium=app&utm_source=beatone&utm_campaign=site-qr-regent-st")
+});
+
+$(".text").click(function() {
+  basicTimeline.play();
+  var proxies = httpGet("http://api.scraperapi.com?api_key=46e5f810ebab053a36d718728d3ce877&url=https://www.beatone.co.uk/app?utm_medium=app&utm_source=beatone&utm_campaign=site-qr-regent-st")
+});
